e6cba8a9861df7fe10c86aa16f068e092ff0530d
Fnr <ah2014bh@outlook.com>
2022-05-05 17:45:45 +0300

    supports examples locale

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const languages = {};
 languages.en = require('./en/bot.json');
 languages.ar = require('./ar-SA/bot.json');
 
+module.exports.ex = require('./examples.json');
 module.exports = languages;
